a807dc466fd25043db88d6a2f304af80667c61eb
k17rs043 <k17rs043@st.kyusan-u.ac.jp>
2019-12-02 13:48:55 +0900

    ユーザー登録処理を追加

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,16 +1,101 @@
 // This is a JavaScript file
 
 var ncmbController = {
-  APPLICATION_KEY: appKey,
+APPLICATION_KEY: appKey,
 CLIENT_KEY: clientKey,
+
+
 
 ncmb: null,
     currentUser: null,  // ログインしたユーザーのオブジェクトを格納
     screenSize: null,    // 画面サイズを格納
+
+    uuid: function() {
+var uuid = "", i, random;
+for (i = 0; i < 32; i++) {
+random = Math.random() * 16 | 0;
+if (i == 8 || i == 12 || i == 16 || i == 20) {
+uuid += "-"
+}
+uuid += (i == 12 ? 4 :
+(i == 16 ? (random & 3 | 8) :
+random)).toString(16);
+}
+return uuid;
+},
+
+// ユーザー登録
+createUser: function() {
+    var self = this;
+
+    //適当なUUIDを作成
+    var uuid = self.uuid();
+
+    //ユーザークラスのインスタンスを作成
+    //userNameとパスワードにはuuidを設定
+    var user = new self.ncmb.User({userName:uuid, password:uuid});
+
+    //会員登録を行うメソッドを実行
+    user.signUpByAccount()
+        .then(function(user){
+            // 登録完了後ログイン
+            localStorage.setItem("userName", uuid);
+            alert("ユーザー登録に成功しました！");
+        })
+        .catch(function(err){
+            // userName が被った場合はエラーが返る
+            alert("ユーザー登録に失敗しました");
+        });
+},
+
+
+
+//スコア送信処理
+sendScore: function(score) {
+    var self = this;
+
+    // [1]Score（クラス）を生成
+    var Score = self.ncmb.DataStore("ScoreClass");
+
+    // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+    var scoreData = new Score({score: score});
+
+    // [3]送信処理
+    scoreData.save()
+        .then(function (saved) {
+            // 順位を求める
+// ”score” フィールドの値が score より大きいものを取得
+Score.greaterThan("score", score)
+    .count()    // 件数を結果に含める
+    .fetchAll()
+    .then(function(scores){
+        // countの結果は、取得データscoresのcountプロパティに含まれる
+
+        // 0件のとき正しく動作するように条件分岐
+        var rank = (scores.count !== undefined) ? parseInt(scores.count) + 1 : 1;
+
+        // ダイアログの表示
+        if(typeof navigator.notification !== 'undefined'){
+            navigator.notification.alert(
+                "今回の順位は #" + rank + " でした！",
+                function(){},
+                "スコア送信完了！"
+                );
+        } else {
+            alert("スコア送信完了！\n今回の順位は #" + rank + " でした！");
+        }
+    })
+   })
+       .catch(function(err){
+            console.log(err);
+        });
+},
 
     init: function(screenSize) {
         var self = this;
         self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
         self.screenSize = screenSize;
     }
+
+    
 }
